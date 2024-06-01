773d549ff6bfeadf038c230787e77495f9f9c929
Philipp Engel <philipp.engel.1990@googlemail.com>
2024-06-01 20:54:18 +0200

    add checksum type details

--- metalink-downloader/src/utils.rs
+++ metalink-downloader/src/utils.rs
@@ -1,7 +1,9 @@
+use anyhow::Result;
+use digest::generic_array::ArrayLength;
+use digest::{Digest, OutputSizeUser};
+use iana_registry_enums::HashFunctionTextualName;
 use std::path::PathBuf;
 use std::time::Duration;
-
-use anyhow::Result;
 
 pub fn make_http_client(user_agent: String) -> Result<reqwest::Client> {
     Ok(reqwest::ClientBuilder::new()
@@ -33,7 +35,44 @@ impl ChunkMetaData {
 }
 
 #[derive(Debug, PartialEq)]
-pub struct CheckSum;
+pub struct CheckSum {
+    hash_type: HashFunctionTextualName,
+    checksum: String,
+}
+
+fn calculate_checksum<D: Digest>(data: &bytes::Bytes) -> String
+where
+    <D as OutputSizeUser>::OutputSize: std::ops::Add,
+    <<D as OutputSizeUser>::OutputSize as std::ops::Add>::Output: ArrayLength<u8>,
+{
+    format!("{:x}", D::digest(&data))
+}
+
+impl CheckSum {
+    pub fn new(hash_type: HashFunctionTextualName, checksum: String) -> Self {
+        Self {
+            hash_type,
+            checksum,
+        }
+    }
+
+    fn calculate_checksum(&self, data: &bytes::Bytes) -> String {
+        match self.hash_type {
+            HashFunctionTextualName::Md2 => calculate_checksum::<md2::Md2>(&data),
+            HashFunctionTextualName::Md5 => calculate_checksum::<md5::Md5>(&data),
+            HashFunctionTextualName::Sha1 => calculate_checksum::<sha1_checked::Sha1>(&data),
+            HashFunctionTextualName::Sha224 => calculate_checksum::<sha2::Sha224>(&data),
+            HashFunctionTextualName::Sha256 => calculate_checksum::<sha2::Sha256>(&data),
+            HashFunctionTextualName::Sha384 => calculate_checksum::<sha2::Sha384>(&data),
+            HashFunctionTextualName::Sha512 => calculate_checksum::<sha2::Sha512>(&data),
+            _ => unimplemented!(),
+        }
+    }
+
+    pub fn validate_checksum(&self, data: &bytes::Bytes) -> bool {
+        self.calculate_checksum(&data) == self.checksum
+    }
+}
 
 pub fn calculate_ranges(total_size: u64, block_size: u64, filename: PathBuf) -> Vec<ChunkMetaData> {
     let mut remaining_size = total_size;
@@ -93,5 +132,16 @@ mod tests {
                 ChunkMetaData::new(10, 14, "/x".into())
             ]
         );
+    }
+
+    #[test]
+    fn validate_checksum() {
+        let checksum = CheckSum::new(
+            HashFunctionTextualName::Sha256,
+            String::from("ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"),
+        );
+
+        let bytes = bytes::Bytes::from(&b"abc"[..]);
+        assert_eq!(checksum.validate_checksum(&bytes), true);
     }
 }
